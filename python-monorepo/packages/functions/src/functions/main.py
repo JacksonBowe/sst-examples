732ba8c37de531f3d81ff49097cefde3758f6820
JacksonBowe <jacksonbowe98@gmail.com>
2024-10-06 13:23:39 +1000

    somehow it's broken now

--- python-monorepo/packages/functions/src/functions/main.py
+++ python-monorepo/packages/functions/src/functions/main.py
@@ -1,12 +1,10 @@
-from core import hello
+
 
 from core import sum
-from core.test.hello import test_hello
 
 print("HHHHHHHHHHHHHHHHHHHHHHHH")
 
 
 def handler(event, context):
     print("Hello from function")
-    test_hello()
     return
